116c8fccbe0b8fa5b5fb152317bf954d587a13c6
sakibsarker <sakib15-3824@diu.edu.bd>
2023-04-03 23:49:07 +0600

    categories add on homepage

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -1,9 +1,45 @@
 import React from 'react'
 import { STATUS } from '../../utils/status'
 import './Category.scss'
-const Category = () => {
+import { Link } from 'react-router-dom'
+import Error from '../Error/Error'
+import Loader from '../Loader/Loader'
+const Category = ({categories,status}) => {
+  if(status===STATUS.Error)return(<Error/>)
+  if(status===STATUS.LOADING)return(<Loader/>)
+  console.log(categories)
   return (
-    <div>Category</div>
+    <section className='categories py-5 bg-ghost-white' id='categories'>
+      <div className="container">
+        <div className="categories-content">
+          <div className="section-title">
+            <h3 className="text-uppercase fw-7 text-regal-blue ls-1">
+              Category
+            </h3>
+            <div className="category-items grid">
+              {
+                categories.slice(0,5).map(category=>(
+                  <Link to={`/categories/${category.id}`} key={category.id}>
+                  <div className="category-item">
+                    <div className="category-item-img">
+                      <img src={category.image} alt={category.name}/>
+                    </div>
+                    <div className="category-item-name text-center">
+                      <h6 className='fs-20'>{category.name}</h6>
+
+                    </div>
+                  </div>
+                  </Link>
+                ))
+              
+              }
+
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </section>
   )
 }
 
